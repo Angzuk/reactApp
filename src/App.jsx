ebde63d8942ee61201fc88d194ae7f299616aa63
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-22 12:16:23 +0100

    Adding Navbar
    with 3 elements (div) Home, Modules, Students

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ function App() {
 
   return (
     <div className="layout">
+
       <header>
         <h1>Basic React Demo</h1>
         <p className="welcome">Welcome {loggedInUser}</p>
       </header>
+
+      <nav>
+        <div className="navItem">
+          <a to="/">Home</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/modules">Modules</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/students">Students</a>
+        </div>
+      </nav>
 
     </div>
 )
