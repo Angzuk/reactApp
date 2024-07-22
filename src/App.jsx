7187ee28e6706280e1843fe27045c3e3680e1ab0
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-22 12:12:21 +0100

    Adding first jsx instead of hardcoded data
    Removing hardcoded Graeme and adding it to jsx (javascript variable)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss';
 function App() {
+  const loggedInUser = "Graeme";
 
   return (
     <div className="layout">
       <header>
         <h1>Basic React Demo</h1>
-        <p className="welcome">Welcome Graeme</p>
+        <p className="welcome">Welcome {loggedInUser}</p>
       </header>
 
     </div>
