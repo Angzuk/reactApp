e9b1d092043f5ea4b8752d36d1b9e710f9163e03
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-22 23:04:24 +0100

    Students that we populate from a flat file
    Create a new object
    Build a handler
    Put button in and attached the handler to the button so when its clicked we add a student
    Adding useState so it detects when state is changing.

--- src/components/views/Students.jsx
+++ src/components/views/Students.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CardContainer, Card } from '../UI/Card.jsx';
 import './Students.scss';
 
@@ -16,27 +17,52 @@ const studentlist= [
 
 function Students (){
   // Initialisation -------------------------------------------
+  const newStudent = {
+    UserFirstname: "Hashim", 
+    UserLastname: "ABDALLAH", 
+    UserEmail: "K1083353@kingston.ac.uk", 
+    UserRegistered: 0, 
+    UserLevel: 4, 
+    UserYearID: 1, 
+    UserUsertypeID: 2, 
+    UserImageURL: 
+      "https://images.generated.photos/eL1-OlKDqGf1IaL_b2O8aSj7osDX_eFVHZEoJ0f3ZV0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzYwNjc0LmpwZw.jpg", 
+    UserUsertypeName: "Student", 
+    UserYearName: "2022-23" ,
+  };
   // State ----------------------------------------------------
+  // when you use useState react will keep a track if that variable changes
+  const [students, setStudents] = useState(studentlist);
+
   // Handlers -------------------------------------------------
+  const handleAdd = (student) => {
+    student.UserID = Math.floor(10000 * Math.random());
+    setStudents([...students, newStudent]);
+    console.log(`Length of students: ${students.length}` );
+  };
   // View -----------------------------------------------------  
     return(
         <>
         <h1>Students</h1>
           <CardContainer>
           {
-            studentlist.map((student)=>{
+            students.map((student)=>{
               return(
                 <div className="studentCard" key={student.UserEmail}>
                   <Card>
                     <p>{student.UserEmail.substring(0,8)}</p>
                     <p>{`${student.UserFirstname} ${student.UserLastname}`}</p>
-                    <img src={student.UserImageURL} />
+                    <img 
+                      src={student.UserImageURL} 
+                      alt={student.UserEmail.substring(0,8)}
+                    />
                   </Card>
                 </div>
             )
             })
           }
           </CardContainer>
+          <button onClick={() => handleAdd(newStudent)}>Add student</button>
           </>
     );
 }
