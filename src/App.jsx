5fa529729f1a8b4910744e3bdd7a06ae9b876342
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-22 17:20:54 +0100

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout.jsx';
 import Home from './components/views/Home.jsx';
 import Modules from './components/views/Modules.jsx';
@@ -8,11 +9,15 @@ function App() {
  
 
   return (
-    <Layout loggedInUser={loggedInUser}>
-      <Home />
-      <Modules />
-      <Students />
-    </Layout>
+    <BrowserRouter>
+      <Layout loggedInUser={loggedInUser}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/modules" element={<Modules />} />
+          <Route path="/students" element={<Students />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
 );
 }
 
