07677995caa2210f5a1cbaedf705792cc3a7ef3a
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-22 11:56:22 +0100

    Adding header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ function App() {
 
   return (
     <div className="layout">
-      
+      <header>
+        <h1>Basic React Demo</h1>
+        <p className="welcome">Welcome Graeme</p>
+      </header>
+
     </div>
 )
 }
