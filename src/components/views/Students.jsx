8cf395ad019385020b5d265e89bdbdacb930a588
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-23 17:21:41 +0100

    Load data from the API using Fetch API
    Loading data using apiGet and applying useEffect hook so that the page is not re-render endlessly.
    Fetch API its a method that can reach to the backend endpoint and apply the get method.

--- src/components/views/Students.jsx
+++ src/components/views/Students.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CardContainer, Card } from '../UI/Card.jsx';
 import './Students.scss';
 
 function Students (){
-
   // Initialisation -------------------------------------------
   const newStudent = {
     UserFirstname: "Hashim", 
     UserLastname: "ABDALLAH", 
-    UserEmail: "K1083353@kingston.ac.uk", 
+    UserEmail: "K1083454@kingston.ac.uk", 
     UserRegistered: 0, 
     UserLevel: 4, 
     UserYearID: 1, 
@@ -19,9 +18,23 @@ function Students (){
     UserYearName: "2022-23" ,
   };
 
+  const loggedInuserGroup = 13;
+  const apiURL = 'https://softwarehub.uk/unibase/api';
+  // const apiURL = `http://localhost:5000/api`;
+  const myGroupEndpoint = `${apiURL}/users/groups/${loggedInuserGroup}`;
+
   // State ----------------------------------------------------
   // when you use useState react will keep a track if that variable changes
   const [students, setStudents] = useState(null);
+  
+  const apiGet = async (endpoint) => {
+  // trying to update the state with the response from the API
+  const response = await fetch(endpoint); // waits till get the response from the API
+  const result = await response.json(); // waits till completed converting that json into a javaScript object and put into results 
+  setStudents(result); // updates the state
+  }
+
+  useEffect(()=> {apiGet(myGroupEndpoint)}, [myGroupEndpoint]); // useEffect is usefull to stop rendering endlesly
 
   // Handlers -------------------------------------------------
   const handleAdd = (student) => {
