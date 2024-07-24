206adfbf4b1299693b8f9eaca7851ca3d3818e59
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-07-24 14:53:22 +0100

    Adding Conformance - converting values from Html2js and Js2Html
    - "conformance.html2js" when we are converting from the form value back into javaScript object
    - "conformance.js2html" when we are going from javaScript object into something that we can use in the form

--- src/components/entity/module/ModuleForm.jsx
+++ src/components/entity/module/ModuleForm.jsx
@@ -14,23 +14,33 @@ const initialModule = {
 
 function ModuleForm({onCancel}) {
     // Initialisation -------------------------------------------
-
-    const html2js = {
-        // converting html values from the form into javaScript objects
-        ModuleName: (value) => (value === '' ? null : value), // if empty string return null if no return value
-        ModuleCode: (value) => (value === '' ? null : value),
-        ModuleLevel: (value) => parseInt(value), // integer
-        ModuleYearID: (value) => (value === 0 ? null : parseInt(value)), // this is the approach for the key
-        ModuleLeaderID: (value) => (value === 0 ? null : parseInt(value)),
-        ModuleImageURL: (value) => (value === '' ? null : value),
-    }
+    const conformance = {
+        html2js: {
+            // converting html values from the form into javaScript objects
+            ModuleName: (value) => (value === '' ? null : value), // if empty string return null if no return value
+            ModuleCode: (value) => (value === '' ? null : value),
+            ModuleLevel: (value) => parseInt(value), // integer
+            ModuleYearID: (value) => (value === 0 ? null : parseInt(value)), // this is the approach for the key
+            ModuleLeaderID: (value) => (value === 0 ? null : parseInt(value)),
+            ModuleImageURL: (value) => (value === '' ? null : value),
+        },
+        js2html: {
+            // converting javaScript values from the form into html objects
+            ModuleName: (value) => (value === null ? "" : value),
+            ModuleCode: (value) => (value === null ? "" : value),
+            ModuleLevel: (value) => value,
+            ModuleYearID: (value) => (value === null ? 0 : value), 
+            ModuleLeaderID: (value) => (value === null ? 0 : value),
+            ModuleImageURL: (value) => (value === null ? "" : value),
+        }
+}
     // State ----------------------------------------------------
     const [module, setModule] = useState(initialModule);
 
     // Handlers -------------------------------------------------
     const handleChange = (event) => {
         const { name, value } = event.target;
-        setModule({...module, [name]: html2js[name](value) }); // overriding the field that change with the new value
+        setModule({...module, [name]: conformance.html2js[name](value) }); // overriding the field that change with the new value
         // so we create an object with all old values (remember that one of this values has changed)
         // and all the old values are being overrided with the ([name]: value) new values
     };
@@ -48,7 +58,7 @@ function ModuleForm({onCancel}) {
                     <input 
                         type="text" 
                         name="ModuleName" 
-                        value={module.ModuleName === null ? "" : module.ModuleName} // converting javaScript into HTML form 
+                        value={conformance.js2html["ModuleName"](module.ModuleName)} // converting javaScript into HTML form 
                         onChange={handleChange}
                     />
                 </label>
@@ -58,14 +68,14 @@ function ModuleForm({onCancel}) {
                     <input 
                         type="text" 
                         name="ModuleCode" 
-                        value={module.ModuleCode === null ? "" : module.ModuleCode}
+                        value={conformance.js2html["ModuleCode"](module.ModuleCode)}
                         onChange={handleChange}
                     />
                 </label>
 
                 <label>
                     Module Level
-                    <select name="ModuleLevel" value={module.ModuleLevel} onChange={handleChange}>
+                    <select name="ModuleLevel" value={conformance.js2html["ModuleLevel"](module.ModuleLevel)} onChange={handleChange}>
                         <option value="0" disabled>None selected</option>
                         {
                             [3,4,5,6,7].map((level) => 
